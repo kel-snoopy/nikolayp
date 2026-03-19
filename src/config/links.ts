3c5517b6f276b60729b3beefc12bbdc140f7f0a4
Sergey Skirev <seregaskirev@gmail.com>
2026-03-20 01:18:49 +0500

    hero

---
+++ src/config/links.ts
@@ -0,0 +1,8 @@
+export const profileLinks = {
+  cvUrl: "https://drive.google.com/file/d/17ssZK6m1n5E03IY5ayUKIM8S82joZaFt/view",
+  linkedInUrl: "https://linkedin.com/in/nickposvezhnyi",
+  aboutUrl: "/about",
+  email: "kelsnoopy@gmail.com",
+  telegramUrl: "https://t.me/kelsnoopy",
+} as const;
+
